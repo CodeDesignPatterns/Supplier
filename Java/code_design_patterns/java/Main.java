ef9ff186c82c919d542a6e38c9536bb9ad4de741
Unknown3134 <S3791201@student.rmit.edu.au>
2022-08-01 13:35:09 +1000

    Formatting

--- Java/code_design_patterns/java/Main.java
+++ Java/code_design_patterns/java/Main.java
@@ -1,18 +1,17 @@
 package code_design_patterns.java;
 
-
 import java.util.function.Supplier;
 
 public class Main {
 
+  // Member variables cause in a more complex example, we could get new tacos often using the Supplier interface.
   private static Supplier<Taco> tacoSupplierChicken = () -> new Taco("Chicken");
   private static Supplier<Taco> tacoSupplier = () -> new Taco();
-
- private static Supplier<String> tacoHello = () -> "Hello, welcome to a test of the Supplier interface";
+  private static Supplier<String> tacoHello = () -> "Hello, welcome to a test of the Supplier interface";
 
   public static void main(String[] args) {
 
-    //Hello world to begin
+    // Hello world to begin
     System.out.println(tacoHello.get());
 
     // Temp taco, calling Chicken supplier
@@ -20,7 +19,7 @@ public class Main {
     // Print it out
     System.out.println(taco.toString());
 
-    //This time call default supplier, no meat, straight to print out.
+    // This time call default supplier, no meat, straight to print out.
     System.out.println(tacoSupplier.get().toString());
 
   }
