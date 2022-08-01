ed130fa9d5da39a1eb1b4f29398550cf56873e26
Unknown3134 <S3791201@student.rmit.edu.au>
2022-08-01 13:34:15 +1000

    Formatting

--- Java/code_design_patterns/java/Taco.java
+++ Java/code_design_patterns/java/Taco.java
@@ -5,22 +5,17 @@ public class Taco {
   private String meat;
   private static final String DEFAULT_MEAT = "Beef";
 
-
   public Taco(String meat) {
     this.meat = meat;
-
   }
 
   public Taco() {
     this.meat = DEFAULT_MEAT;
-
   }
 
   public String getMeat() {
     return meat;
   }
-
-  
 
   @Override
   public String toString() {
